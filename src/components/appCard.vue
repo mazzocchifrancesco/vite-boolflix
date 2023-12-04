72467986312a1cdce350f7b5a23276e2d0391d60
FrancescoMazzocchi <123@email.it>
2023-12-04 18:41:59 +0100

    add flags

--- src/components/appCard.vue
+++ src/components/appCard.vue
@@ -1,7 +1,6 @@
 <script>
 import { store } from "../store.js"
 
-
 export default {
     name: "appCard",
     props: {
@@ -12,14 +11,25 @@ export default {
             store,
         }
     },
+    methods: {
+        getImageUrl(name) {
+            if (name == "en") {
+                name = 'gb';
+            };
+            return new URL(`../../node_modules/svg-country-flags/svg/${name}.svg`, import.meta.url).href
+        }
+    }
 }
+
 </script>
 
 <template>
     <div id="card" class=" d-flex flex-column flex-wrap bg-white">
         <h4> {{ info.title }}</h4>
         <p> {{ info.original_title }}</p>
-        <p>{{ info.original_language }}</p>
+        <p> {{ info.original_language }}</p>
+
+        <img :src="getImageUrl(info.original_language)" alt=aaaaa>
         <p>{{ info.vote_average }}</p>
     </div>
 </template>
@@ -28,5 +38,9 @@ export default {
     width: 30%;
     margin-right: calc(10% / 3);
     margin-bottom: 1rem;
+}
+
+img {
+    width: 2rem;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -4,5 +4,6 @@ export const store = reactive({
     apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=",
     apiKey: "73186e0865f544d82855e0f04703c7c6",
     searchString: "",
-    movies: []
+    movies: [],
+    // imgString: `../../node_modules/svg-country-flags/svg/en.svg`
 })
